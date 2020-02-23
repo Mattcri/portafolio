b8c55daac740918e6857d5ce4caff8d00e309f2f
Mcriado <mcriado.cg@gmail.com>
2020-02-23 01:11:02 -0300

    Animaciones js y cambios en el css

--- js/script.js
+++ js/script.js
@@ -2,5 +2,52 @@ $(document).ready(function(){
    
    $('.btn-about-me').click(function(){
       $('#bio').slideToggle('slow');
-   });  
+   });
+   
+   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+   
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+   });
+
+   window.msr = ScrollReveal();
+      msr.reveal('.navbar', {
+      duration: 2000,
+      origin: 'bottom',
+      distance: '20px'
+      });
+      msr.reveal('.h-img', {
+         duration: 2000,
+         origin: 'top',
+         distance: '200px'
+      });
+      msr.reveal('.h-content', {
+         duration: 2000,
+         origin: 'right',
+         distance: '300px'
+      });
+      msr.reveal('.btn-about-me', {
+         duration: 2000,
+         origin: 'bottom',
+         distance: '10px',
+         delay: 1000
+      });
+      msr.reveal('.wrap-ring-and-rebel', {
+         duration: 2000,
+         origin: 'left',
+         distance: '300px',
+         delay: 800
+      });
+      msr.reveal('.wrap-front', {
+         duration: 2000,
+         origin: 'right',
+         distance: '300px',
+         delay: 800
+      });
+
 });
+
